583122449036395b58a4da1e426a87f13abe5c41
Kanu3690 <kanu3690@gmail.com>
2025-03-27 17:21:00 +0530

    2 section updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import Textlogo from "../assets/text-logo.svg";
+import Logo from "../assets/Logo.svg";
+import LogoMenu from "../assets/logo-menu.svg";
+import MenuIcon from "../assets/menu.svg"; // Custom menu icon
+import CloseIcon from "../assets/close.svg"; // Custom close icon
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <header className="bg-gray-800 text-white p-4">
-            <nav className="flex justify-between items-center">
-                <div className="font-bold text-xl">Language King</div>
-                <ul className="flex space-x-4">
-                    <li>Courses</li>
-                    <li>Testimonials</li>
-                    <li>Get a Callback</li>
-                </ul>
-            </nav>
+        <header className="bg-black text-white sm:pt-2 sm:pb-4">
+            <div className="custom-container mx-auto px-4">
+                <div className="flex items-center justify-between">
+                    {/* Left Logo */}
+                    <div className="xl:w-[40%] w-[30%] lg:flex hidden">
+                        <img src={Textlogo} alt="Logo" className="w-40 md:w-auto" />
+                    </div>
+
+                    {/* Right Logo */}
+                    <div className="xl:w-[20%] sm:w-[30%] w-[50%] overflow-hidden">
+                        <img src={Logo} alt="Logo" className="xl:ml-0 ml-[-27%]" />
+                    </div>
+
+                    <div className="xl:w-[40%] w-[50%] flex justify-end items-center">
+                        {/* Hamburger Menu - Visible on Mobile */}
+                        <button
+                            className="lg:hidden flex items-center justify-center p-3 bg-chinese-Black border border-davys-Grey focus:outline-none cursor-pointer"
+                            onClick={() => setIsOpen(!isOpen)}>
+                            <img src={isOpen ? CloseIcon : MenuIcon} alt="Menu Toggle" className="w-8 h-8" />
+                        </button>
+
+                        {/* Desktop Navigation */}
+                        <nav className="hidden lg:flex justify-end items-center ">
+                            <ul className="flex gap-[60px]">
+                                <li className="text-center inline-flex items-center"><a href="#" className="text-white inline-flex items-center justify-center font-bold text-[20px] border-b-2 border-transparent hover:border-white transition-all duration-300">Courses</a></li>
+                                <li className="text-center inline-flex items-center"><a href="#" className="text-white inline-flex items-center justify-center font-bold text-[20px] border-b-2 border-transparent hover:border-white transition-all duration-300">Testimonials</a></li>
+                                <li className="text-center inline-flex items-center">
+                                    <a href="#" className="text-white inline-flex items-center justify-center border border-white/25 hover:border-white px-6 py-3 font-bold text-[18px] transition-all duration-300">
+                                        Get a Callback
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+
+                </div>
+
+                {/* Mobile Menu (Dropdown) */}
+                {isOpen && (
+                    <nav className="lg:hidden fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-gray7 z-50 py-[45px] px-4">
+                        <div className="flex items-center justify-end">
+                            <button className="flex items-center justify-center p-3 bg-chinese-Black border border-davys-Grey cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+                                <img src={isOpen ? CloseIcon : MenuIcon} alt="CloseIcon" className="w-5" />
+                            </button>
+                        </div>
+                        <ul className="flex flex-col justify-center items-center gap-[34px]">
+                            <li className="text-center inline-flex items-center"><a href="#" className="text-white inline-flex items-center justify-center font-bold text-[32px] border-b-2 border-transparent hover:border-white transition-all duration-300">Courses</a></li>
+                            <li className="text-center inline-flex items-center"><a href="#" className="text-white inline-flex items-center justify-center font-bold text-[32px] border-b-2 border-transparent hover:border-white transition-all duration-300">Testimonials</a></li>
+                            <li className="text-center inline-flex items-center"><a href="#" className="text-white inline-flex items-center justify-center font-bold text-[32px] border-b-2 border-transparent hover:border-white transition-all duration-300">Contact us</a></li>
+                        </ul>
+
+                        <div className="flex flex-col justify-center items-center py-[70px] gap-1">
+                            <a href="mailto:info@languageking.com.au" className="text-[#9D9B9B] font-medium text-base">info@languageking.com.au</a>
+                            <span className="text-white font-medium text-[12px]">Copyright @ 2025 Language King</span>
+                        </div>
+
+                        <img src={LogoMenu} alt="Logo" className="max-w-[130px] mx-auto" />
+                    </nav>
+                )}
+            </div>
         </header>
     );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,117 @@
 import React from "react";
+import CourseImg1 from '../assets/course-img1.png';
+import CourseImg2 from '../assets/course-img2.png';
+import CourseImg3 from '../assets/course-img3.png';
+import avatar from '../assets/avatar-img.png';
 
 const Home = () => {
     return (
-        <main className="">
-            <h2 className="text-yellow-400 text-xl"># Australia’s No.1</h2>
-            <h1 className="text-4xl font-bold mt-2">
-                PTE, IELTS & NAATI CCL <br /> Coaching Institute
-            </h1>
-            <p className="text-gray-400 mt-4">
-                Join 10,000+ satisfied students in achieving their desired Test Scores.
-                Helped many students, now it's your turn!
-            </p>
-            <div className="mt-6 flex justify-center space-x-4">
-                <button className="bg-gradient-to-r from-yellow-400 to-red-500 text-black px-6 py-3 rounded">
-                    Get a Callback
-                </button>
-                <button className="border border-yellow-400 px-6 py-3 rounded">
-                    Watch Free Lessons
-                </button>
-            </div>
+        <main>
+            <section className="bg-black lg:pt-[180px] lg:pb-[230px] md:pt-[100px] md:pb-[100px] pt-[90px] pb-[50px]">
+                <div className="custom-container mx-auto px-3">
+                    <div className="max-w-[830px]">
+                        <h2 className="text-gradient-primary font-bold md:text-[32px] text-2xl"># Australia’s No.1</h2>
+                        <h1 className="font-inter font-bold lg:text-[72px] sm:text-[58px] text-[36px] lg:leading-[77px] sm:leading-[66px] leading-[40px] sm:mt-0 mt-3 text-white">
+                            PTE, IELTS & NAATI CCL  Coaching Institute
+                        </h1>
+                        <p className="md:text-2xl sm:text-xl text-base font-light max-w-[800px] text-white/75 mt-5">
+                            Join 10,000+ satisfied students in achieving their desired Test Scores.
+                            Helped many students, now it's your turn!
+                        </p>
+                        <div className="mt-[48px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[800px]">
+                            <a href="#" className="primary-btn bg-gradient-primary leading-[28px] inline-flex justify-center items-center text-center text-black px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl relative z-10">
+                                Get a Callback
+                            </a>
+                            <a href="#" className="text-center bg-transparent border-2 border-Cadmium-Orange text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl transition-all duration-300 ease hover:bg-white/10">
+                                Watch Free Lessons
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="bg-black">
+                <div className="custom-container mx-auto px-3 py-[38px] border-t border-[#252525]">
+                    <h2 className="text-white font-bold sm:text-[29.39px] text-2xl relative  inline-block z-10 mb-[38px] stroke-heading">Our Courses</h2>
+
+                    <div className="flex flex-col gap-[38px]">
+                        <div class="grid lg:grid-cols-2 gap-[38px] bg-[#121211] transition-all duration-300 ease hover:bg-white/10 border border-white/10 sm:py-[31px] py-[20px] sm:px-[38px] px-[20px] group relative">
+                            <div class="ribbon ribbon-top-left z-10"><span>New course!</span></div>
+                            <div className="overflow-hidden relative z-0">
+                                <img src={CourseImg1} alt="CourseImg1" className="w-full h-full object-cover transition-all duration-300 ease group-hover:scale-[1.05]" />
+                            </div>
+                            <div className="lg:py-[40px]">
+                                <h2 className="text-gradient-primary font-bold md:text-[26px] inline-block sm:text-2xl text-lg">NAATI CCL <span className="text-white">Fast Track</span> Course</h2>
+                                <h2 className="font-bold lg:text-[46px] sm:text-[32px] text-2xl text-white lg:leading-[50px] sm:leading-[38px] leading-[28px] my-[18px]">Need to clear the test in 3 days, we’ve got you covered!</h2>
+                                <p className="md:text-xl lg:text-lg text-base font-light max-w-[800px] text-white/75 mb-5">
+                                    This is a super fast-paced course. It’s geared for speed, designed for those who need to clear the test on urgent basis, and do whatever it takes to clear it.
+                                </p>
+                                <div className="flex items-center gap-3">
+                                    <img src={avatar} alt="avatar" className="w-[50px] h-[50px] rounded-full border-4 border-white/10" />
+                                    <h3 className="text-xl text-white/60">Taught by <span className="text-white font-bold">Abhishek</span></h3>
+                                </div>
+                                <div className="sm:mt-[48px] mt-[24px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[800px]">
+                                    <a href="#" className="primary-btn bg-gradient-primary leading-[28px] inline-flex justify-center items-center text-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl relative z-10">
+                                        Take the Course
+                                    </a>
+                                    <a href="#" className="text-center bg-transparent border-2 border-white/20 inline-flex justify-center items-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl transition-all duration-300 ease hover:bg-white/10">
+                                        View Details
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid lg:grid-cols-2 gap-[38px] bg-[#121211] transition-all duration-300 ease hover:bg-white/10 border border-white/10 sm:py-[31px] py-[20px] sm:px-[38px] px-[20px] group relative">
+                            <div className="overflow-hidden relative z-0">
+                                <img src={CourseImg2} alt="CourseIm21" className="w-full h-full object-cover transition-all duration-300 ease group-hover:scale-[1.05]" />
+                            </div>
+                            <div className="lg:py-[40px]">
+                                <h2 className="text-gradient-secondary font-bold md:text-[26px] inline-block sm:text-2xl text-lg">PTE Master Class</h2>
+                                <h2 className="font-bold lg:text-[46px] sm:text-[32px] text-2xl text-white lg:leading-[50px] sm:leading-[38px] leading-[28px] my-[18px]">Missed your score by a few marks! Clear in 2 weeks</h2>
+                                <p className="md:text-xl lg:text-lg text-base font-light max-w-[800px] text-white/75 mb-5">
+                                    This is literally the best advice that I give every student. My strategies will teach you how to get 90 in each module. I’ve helped students out for 5+ years as a super-friendly teacher to get their desired score, now it’s your turn.
+                                </p>
+                                <div className="flex items-center gap-3">
+                                    <img src={avatar} alt="avatar" className="w-[50px] h-[50px] rounded-full border-4 border-white/10" />
+                                    <h3 className="text-xl text-white/60">Taught by <span className="text-white font-bold">Abhishek</span></h3>
+                                </div>
+                                <div className="sm:mt-[48px] mt-[24px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[800px]">
+                                    <a href="#" className="primary-btn bg-gradient-secondary leading-[28px] inline-flex justify-center items-center text-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl relative z-10">
+                                        Take the Course
+                                    </a>
+                                    <a href="#" className="text-center bg-transparent border-2 border-white/20 inline-flex justify-center items-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl transition-all duration-300 ease hover:bg-white/10">
+                                        View Details
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid lg:grid-cols-2 gap-[38px] bg-[#121211] transition-all duration-300 ease hover:bg-white/10 border border-white/10 sm:py-[31px] py-[20px] sm:px-[38px] px-[20px] group relative">
+                            <div className="overflow-hidden relative z-0">
+                                <img src={CourseImg3} alt="CourseImg3" className="w-full h-full object-cover transition-all duration-300 ease group-hover:scale-[1.05]" />
+                            </div>
+                            <div className="lg:py-[40px]">
+                                <h2 className="dark-text-custom font-bold md:text-[26px] block sm:text-2xl text-lg">PTE Master Handbook</h2>
+                                <h2 className="font-bold lg:text-[46px] sm:text-[32px] text-2xl text-white lg:leading-[50px] sm:leading-[38px] leading-[28px] my-[18px]">PTE 79+ in 7 days
+                                    Crack PTE in 1  attempt</h2>
+                                <p className="md:text-xl lg:text-lg text-base font-light max-w-[800px] text-white/75 mb-5">
+                                    Learn exactly what students did to achieve their desired band. This book explains the exam structure, target scores, key tasks to focus on, and the 7-day strategy you need to follow to go from 5 to 8 each as quickly as possible.
+                                </p>
+                                <div className="flex items-center gap-3">
+                                    <img src={avatar} alt="avatar" className="w-[50px] h-[50px] rounded-full border-4 border-white/10" />
+                                    <h3 className="text-xl text-white/60">Taught by <span className="text-white font-bold">Abhishek</span></h3>
+                                </div>
+                                <div className="sm:mt-[48px] mt-[24px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[800px]">
+                                    <a href="#" className="primary-btn bg-gradient-dark leading-[28px] inline-flex justify-center items-center text-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl relative z-10">
+                                        Free E-Book
+                                    </a>
+                                    <a href="#" className="text-center bg-transparent border-2 border-white/20 inline-flex justify-center items-center text-white px-6 py-5 sm:w-[50%] w-full font-bold sm:text-2xl text-xl transition-all duration-300 ease hover:bg-white/10">
+                                        View Details
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 };
