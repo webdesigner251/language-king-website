5fa26bf794c4a9a14dc8d388af83b1274f328a86
Kanu3690 <kanu3690@gmail.com>
2025-05-07 18:48:04 +0530

    Home and PTE page updated

--- src/pages/PTE-MasterClass.jsx
+++ src/pages/PTE-MasterClass.jsx
@@ -14,7 +14,7 @@ const PTEMasterClass = () => {
                                     Missed your score! <br />
                                     Clear in 2 weeks
                                 </h1>
-                                <p className="md:text-xl sm:text-xl text-base font-light max-w-[650px] text-white/75">
+                                <p className="md:text-xl sm:text-xl text-base font-light max-w-[760px] text-white/75">
                                     This is literally the best advice that I give every student. My strategies will teach you how to get 90 in each module. I’ve helped students out for 5+ years as a super-friendly teacher to get their desired score, now it’s your turn.
                                 </p>
                                 <div className="flex items-center gap-3 my-[12px]">
@@ -22,23 +22,25 @@ const PTEMasterClass = () => {
                                     <h3 className="text-base font-light text-white/60">Taught by <span className="text-white font-semibold">Abhishek</span></h3>
                                 </div>
                             </div>
-                            <div className="mt-[48px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[670px]">
+                            <div className="mt-[38px] flex sm:flex-nowrap flex-wrap sm:gap-[31px] gap-[16px] max-w-[600px]">
                                 <a
                                     href="#"
-                                    className="align-middle inline-flex items-center justify-center text-center primary-btn bg-gradient-primary text-black px-3 py-3 sm:w-[50%] w-full font-semibold sm:text-xl text-lg relative z-10"
+                                    className="align-middle inline-flex items-center justify-center text-center primary-btn bg-gradient-secondary text-white px-3 py-3 sm:w-[50%] w-full font-semibold sm:text-xl text-lg relative z-10"
                                 >
-                                    Get a Callback
+                                    Take the course
                                 </a>
-                                <a href="#" className="align-middle inline-flex items-center gap-[25px] justify-center text-center bg-transparent border-2 border-Cadmium-Orange text-white px-3 py-3 sm:w-[50%] w-full font-semibold sm:text-xl text-lg transition-all duration-300 ease hover:bg-white/10">
-                                    <svg width="20" height="20" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M21.93 11.9998L0 23.9253V0.0742188L21.93 11.9998Z" fill="url(#paint0_linear_702_4078)" />
+                                <a href="#" className="align-middle inline-flex items-center gap-[25px] justify-center text-center bg-transparent border-[1.5px] border-[#333434] text-white px-3 py-3 sm:w-[50%] w-full font-semibold sm:text-xl text-lg transition-all duration-300 ease hover:bg-white/10">
+                                    <svg width="21" height="23" viewBox="0 0 21 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20.6718 11.2245L0.647949 22.0227V0.42627L20.6718 11.2245Z" fill="url(#paint0_linear_814_2245)" />
                                         <defs>
-                                            <linearGradient id="paint0_linear_702_4078" x1="0" y1="11.9997" x2="21.93" y2="11.9997" gradientUnits="userSpaceOnUse">
-                                                <stop stopColor="#D7FF53" />
-                                                <stop offset="1" stopColor="#FD4F2B" />
+                                            <linearGradient id="paint0_linear_814_2245" x1="20.6718" y1="-1.24423" x2="18.2482" y2="25.8636" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#17FFBA" />
+                                                <stop offset="0.130208" stop-color="#76B4C8" />
+                                                <stop offset="1" stop-color="#2F57A7" />
                                             </linearGradient>
                                         </defs>
                                     </svg>
+
                                     Watch Free Lessons
                                 </a>
                             </div>
